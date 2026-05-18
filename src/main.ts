cb67cb20fb8f053b123752cf7e424b3a417fafe9
souliyo <tito.vanvilay@yahoo.com>
2026-05-19 01:14:28 +0700

    add docker and change a little bit of logger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Food Delivery API')
     .setDescription('API documentation for the Food Delivery System')
--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,19 +1,22 @@
-import {Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 
 @Injectable()
 export class Loggermiddlware implements NestMiddleware {
-    constructor(private readonly logger: Logger) {}
+  private readonly logger = new Logger('HTTP');
 
-    use(req: Request, res: Response, next: NextFunction){
-        const { method, path } = req;
-        const startTime = new Date().getTime();
-        const { statusCode } = res;
-        const resTime = new Date().getTime();
-        if( statusCode == 200 || statusCode == 201){
-            this.logger.log(`${method} ${path} ${statusCode} - ${resTime - startTime}ms`);
-        }
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, path } = req;
+    const startTime = Date.now();
 
-        next();
-    }
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const resTime = Date.now() - startTime;
+      if (statusCode === 200 || statusCode === 201) {
+        this.logger.log(`${method} ${path} ${statusCode} - ${resTime}ms`);
+      }
+    });
+
+    next();
+  }
 }

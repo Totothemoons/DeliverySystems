1cf2ad2b271a35a96434f1b7d7923c3358172098
souliyo <tito.vanvilay@yahoo.com>
2026-05-28 01:53:09 +0700

    Add PrismaModule import to DriverModule

--- src/driver/driver.module.ts
+++ src/driver/driver.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DriverController } from './driver.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 import { DriverService } from './driver.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [DriverController],
   providers: [DriverService]
 })
